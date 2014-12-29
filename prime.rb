59ca1d8a7bbaf43326660b9a4430257ce981f625
Charlotte Chang <charlottechang@Charlottes-MBP.leandog.net>
2014-12-29 11:24:31 -0500

    Handle zero

--- prime.rb
+++ prime.rb
@@ -1,7 +1,16 @@
+require 'mathn'
+
 class Prime
 
 	def self.nth(number)
-		2
+		handle_zero if number.eql? 0
+		self.first(number).last
+	end
+
+	private
+	
+	def handle_zero
+		raise ArgumentError, 'All your base are belong to us' 
 	end
 
 end
